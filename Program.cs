715e0ff38474f9a1a959f8579986a6f1eb16ec8f
Kacper Michalski <Kacper.Michalski250@gmail.com>
2023-04-08 23:12:38 +0200

    added the most important methods to the warehouse operations

--- Program.cs
+++ Program.cs
@@ -10,7 +10,103 @@ namespace marketMate
     class Program
     {
 
-        static void initialWarehouse()
+        static void createWarehouse()
+        {
+            string path = @"warehouse.txt";
+            StreamWriter sw = File.CreateText(path);
+            sw.Close();
+        }
+        static StreamReader streamReaderWarehouse()
+        {
+            string path = @"warehouse.txt";
+            StreamReader sr = File.OpenText(path);
+            return sr;
+        }
+
+        static StreamWriter streamWriterWarehouse()
+        {
+            string path = @"warehouse.txt";
+            StreamWriter sw = new StreamWriter(path, true);
+            Console.WriteLine("Magazyn został otwarty!");
+            return sw;
+        }
+
+        static string findProductInWarehouse(string id)
+        {
+            StreamReader sr = streamReaderWarehouse();
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] elements = line.Split(' ');
+                if (elements[0] == id)
+                {
+                    sr.Close();
+                    return line + " szt.";
+                }
+            }
+            return "Nie znaleziono takiego produktu";
+        }
+        static int findProductIndexInWarehouse(string id)
+        {
+            StreamReader sr = streamReaderWarehouse();
+            string line = "";
+            int index = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] elements = line.Split(' ');
+                if (elements[0] == id)
+                {
+                    sr.Close();
+                    return index;
+                }
+                index++;
+            }
+            sr.Close();
+            return -1;
+        }
+
+
+        static void addNewProductToWarehouse(string id, string quantity)
+        {
+            StreamWriter sw = streamWriterWarehouse();
+            sw.WriteLine(id + " " + quantity);
+            sw.Close();
+        }
+
+
+        static void removeProductFromWarehouse(string id)
+        {
+            string[] lines = File.ReadAllLines("warehouse.txt");
+            lines = lines.Where((line, index) => index != findProductIndexInWarehouse(id)).ToArray();
+
+            File.WriteAllLines("warehouse.txt", lines);
+        }
+
+
+
+        static void createPriceList()
+        {
+            string path = @"priceList.txt";
+            StreamWriter sw = File.CreateText(path);
+            Console.WriteLine("Cennik został utworzony!");
+            sw.Close();
+        }
+        static StreamReader streamReaderPriceList()
+        {
+            string path = @"priceList.txt";
+            StreamReader sr = File.OpenText(path);
+            Console.WriteLine("Cennik został otwarty!");
+            return sr;
+        }
+
+        static StreamWriter streamWriterPriceList()
+        {
+            string path = @"priceList.txt";
+            StreamWriter sw = new StreamWriter(path, true);
+            Console.WriteLine("Cennik został otwarty!");
+            return sw;
+        }
+        static void initializeWarehouse()
         {
             string path = @"warehouse.txt";
             StreamWriter sw;
@@ -42,6 +138,89 @@ namespace marketMate
             
         }
 
+        static void initializePriceList()
+        {
+            string path = @"priceList.txt";
+            StreamWriter sw;
+
+            if (!File.Exists(path))
+            {
+                sw = File.CreateText(path);
+                Console.WriteLine("Cennik został utworzony!");
+            }
+            else
+            {
+                sw = new StreamWriter(path, true);
+                Console.WriteLine("Cennik został otwarty!");
+            }
+
+            sw.Close();
+            StreamReader sr = File.OpenText(path);
+            string s = "";
+            int i = 1;
+            Console.WriteLine("Zawartość cennika:");
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(i++ + ". " + s);
+            }
+
+            sr.Close();
+        }
+
+        static void listFeatures()
+        {
+            string[] features = { "1. Magazyn", "2. Cennik", "3. Sprzedaż"};
+            foreach (var feature in features)
+            {
+                Console.WriteLine(feature);
+            }
+
+            Console.Write("Wybierz funkcjonalność podając odpowiednią liczbę: ");
+            string selectedFeature = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (selectedFeature)
+            {
+                case "1":
+                    Console.WriteLine("Wybrano magazyn");
+                    string[] warehouseFeatures = { "1. Dodawanie", "2. Usuwanie", "3. Szukanie"};
+                    foreach (var feature in warehouseFeatures)
+                    {
+                        Console.WriteLine(feature);
+                    }
+
+                    Console.Write("Wybierz funkcjonalność podając odpowiednią liczbę: ");
+                    selectedFeature = Console.ReadLine();
+                    Console.WriteLine();
+                    switch (selectedFeature)
+                    {
+                        case "1":
+                            Console.WriteLine("Wybrano dodawanie");
+                            break;
+                        case "2":
+                            Console.WriteLine("Wybrano usuwanie");
+                            break;
+                        case "3":
+                            Console.WriteLine("Wybrano szukanie");
+                            break;
+                        default:
+                            Console.WriteLine("Err!");
+                            break;
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Wybrano cennik");
+                    break;
+                case "3":
+                    Console.WriteLine("Wybrano sprzedaż");
+                    break;
+                default:
+                    Console.WriteLine("Err!");
+                    break;
+            }
+        }
+
+
         static void endOfApp()
         {
             Console.Write("Naciśnij dowolny klawisz");
@@ -50,8 +229,17 @@ namespace marketMate
 
         static void Main(string[] args)
         {
-            initialWarehouse();
+            // initializeWarehouse();
+            // initializePriceList();
 
+            Console.WriteLine("Dzień dobry, z tej strony marketMate!");
+            // listFeatures();
+
+            //Console.Write("Podaj id produktu który chcesz wyszukać: ");
+            //string id = Console.ReadLine();
+            // addNewProductToWarehouse("km421", "42");
+            Console.WriteLine(findProductIndexInWarehouse("km123"));
+            removeProductFromWarehouse("km321");
             endOfApp();
 
         }
